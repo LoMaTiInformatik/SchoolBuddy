53d8214e9579fece0f1416541dece04276e76262
Lois Gilly <39123442+luigicrafter@users.noreply.github.com>
2023-02-13 15:12:17 +0100

    .

---
+++ src/utils/webuntishandler.py
@@ -0,0 +1,12 @@
+import webuntis
+
+def get_lesson_plan(day):
+    # Get whole lesson plan
+    # Args: day (today/tmrw)
+    # Returns lesson plan from webuntis
+
+def get_lesson(day, lessonnum):
+    # Get single lesson
+    # Args: day (today/tmrw), lessonnum (number of the lesson)
+    # Returns single lesson
+
