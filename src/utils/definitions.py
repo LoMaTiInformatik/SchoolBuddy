15292603ab59cf1c7d709fae421b9e62d49a23f3
Max Guwa <max.leon.guwa@isurfstormarn.de>
2023-03-21 00:29:30 +0100

    .

--- src/utils/definitions.py
+++ src/utils/definitions.py
@@ -1,6 +1,6 @@
 errors = {
     "stt-1": "Ich habe dich leider nicht verstanden.",
-    "stt-2": "Fehler s s t 2. Es gab einen Fehler bei der Bearbeitung deiner Anfrage. Bist du mit dem Internet verbunden?",
+    "stt-2": "Fehler s t t 2. Es gab einen Fehler bei der Bearbeitung deiner Anfrage. Bist du mit dem Internet verbunden?",
     "tts-1": "Fehler t t s 1. Es gab einen Fehler bei der Ausgabe der Sprache.",
     "mysql-1": "Fehler mai s q l 1. Es gab einen Fehler mit der Datenbank."
 }
