0ca26c86f382d87207b23412615d32d7bdc35da5
Lois Gilly <39123442+luigicrafter@users.noreply.github.com>
2023-03-23 13:30:35 +0100

    l

--- src/utils/speak.py
+++ src/utils/speak.py
@@ -2,6 +2,17 @@ from TTS.api import TTS
 import sounddevice as sd
 
 def speak(text):
+
+    """
+        Speaks the given text
+
+        Arguments:
+        text: Text to speak (str)
+
+        Returns:
+        error: Error-Code if any (str)
+        value: None (str)
+    """
     tts = TTS("tts_models/de/thorsten/tacotron2-DDC")
     wav = tts.tts(text=text)
     sd.play(wav, 23000)
@@ -11,6 +22,17 @@ def speak(text):
     }
 
 def question(text):
+
+    """
+        Asks a question with the given text
+
+        Arguments:
+        text: Question to ask (str)
+
+        Returns:
+        error: Error-Code if any (str)
+        value: Answertext (str)
+    """
     pass 
 
 def cancel():
