f9ccca59beaeea1b76a33a3d625f691eb7b1d128
Max Leon Guwa <max.leon.guwa@isurfstormarn.de>
2023-04-21 17:04:31 +0200

    .

--- src/commands/helloworld.py
+++ src/commands/helloworld.py
@@ -1,4 +1,6 @@
-
+# Max is working on this
+# Still waiting on Lois to finish up his backend stuff, so I can continue working on this
+# This is only an example command which won't be implemented into the final version
 
 def cmdfunction(cmdtext: str):
     pass
